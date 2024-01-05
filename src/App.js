bd478086a26f603356f24b530f90654834f5996a
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2024-01-05 11:13:21 +0800

    fixed image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import qrimage from "./image-qr-code.png";
 
 function App() {
   return (
     <div className=" flex flex-col h-screen items-center justify-center bg-blue-300 px-7">
       <div className=" md:w-[300px] mx-auto bg-white rounded-[30px] shadow-md overflow-hidden ">
         <div>
-          <img className=" rounded-[30px] p-5 " src="image-qr-code.png"></img>
+          <img className=" rounded-[30px] p-5 " src={qrimage} />
           <h1 className=" font-bold text-center p-5 ">
             Improve your front-end skills by building projects
           </h1>
