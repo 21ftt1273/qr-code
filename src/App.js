76323bc9201a154eda479efa4c94a1d8df3bb48b
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2024-01-05 10:58:21 +0800

    rahhhhhhhhhh

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" flex flex-col h-screen items-center justify-center bg-blue-300 px-7">
+      <div className=" md:w-[300px] mx-auto bg-white rounded-[30px] shadow-md overflow-hidden ">
+        <div>
+          <img
+            className=" rounded-[30px] p-5 "
+            src="../images/image-qr-code.png"
+          ></img>
+          <h1 className=" font-bold text-center p-5 ">
+            Improve your front-end skills by building projects
+          </h1>
+          <h2 className=" text-center p-5 ">
+            Scan the QR code to visit Frontend Mentor and take your coding to
+            the next level
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
