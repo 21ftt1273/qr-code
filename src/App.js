08b0069fe51471a61c45a4bf4f109373a3f9a1a5
Amirul Aiman <21FTT1273@student.pb.edu.bn>
2024-01-05 11:05:19 +0800

    image

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
     <div className=" flex flex-col h-screen items-center justify-center bg-blue-300 px-7">
       <div className=" md:w-[300px] mx-auto bg-white rounded-[30px] shadow-md overflow-hidden ">
         <div>
-          <img
-            className=" rounded-[30px] p-5 "
-            src="../images/image-qr-code.png"
-          ></img>
+          <img className=" rounded-[30px] p-5 " src="image-qr-code.png"></img>
           <h1 className=" font-bold text-center p-5 ">
             Improve your front-end skills by building projects
           </h1>
